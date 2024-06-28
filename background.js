d4440edbeeb9b50a299fdfed7aa7eb2039996781
Stelio Kontos <37424493+Ste1io@users.noreply.github.com>
2024-06-28 14:32:45 -0500

    Adjust site loading to every 23 hours

--- background.js
+++ background.js
@@ -2,10 +2,10 @@
 // https://meta.stackexchange.com/help/badges/53/fanatic
 
 const url = 'https://stackoverflow.com/users/9412456/stelio-kontos';
-const hr = 23, min = 59;
+const hr = 23, min = 0;
 // const hr = 0, min = 1; // for testing
 
-const timeoutPeriod = (((hr * 60 * 60) + (min * 60) + 0) * 1000) - 10; // offset by -1/100th sec to ensure the alarm fires inside the correct 1-minute interval, given the api's 1-minute periodicity
+const timeoutPeriod = ((hr * 60 * 60) * 1000) - 10; // offset by -1/100th sec to ensure the alarm fires inside the correct 1-minute interval, given the api's 1-minute periodicity
 const closeTabDelay = 2000; // leave the site open briefly, just to cut down on the spookiness factor
 
 chrome.runtime.onInstalled.addListener(() => {
